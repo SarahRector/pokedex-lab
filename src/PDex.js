6c63eb1d6e6d5dda80568ba5095ce2abafa41ee5
SarahRector <rector.sarah.r@gmail.com>
2020-08-05 18:06:34 -0700

    added more attributes and images

--- src/PDex.js
+++ src/PDex.js
@@ -11,8 +11,13 @@ export default class PDex extends React.Component {
         return (
             <div> 
                  {
-                        this.props.pokeState.map(poke => <PokeItem key={poke._id} pokemon={poke.pokemon} />)
-                } 
+                    
+                        this.props.pokeState.map(poke => <PokeItem key={poke._id} pokemon={poke.pokemon} image={poke.url_image} ability={poke.ability_1} type={poke.type_1}/>)
+                 }
+
+
+                        
+            
             </div>
         )
     }
--- src/PokeItem.js
+++ src/PokeItem.js
@@ -5,6 +5,9 @@ export default class PokeItem extends Component {
         return (
             <div>
                 <p>{this.props.pokemon}</p>
+                <img src={this.props.image} alt='pokemon'></img>
+                <p>Main Ability: {this.props.ability}</p>
+                <p>Type: {this.props.type}</p>
             </div>
         )
     }
